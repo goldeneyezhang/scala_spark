d55f954b128c64b8a887e54e6588d579bcacb95c
zhangyibo <goldeneyezhang@163.com>
2017-08-14 20:54:43 -0700

    recommend

---
+++ src/main/scala/com/cainiaobangbang/scalamvn/Recommend.scala
@@ -0,0 +1,15 @@
+package com.cainiaobangbang.scalamvn
+import java.io.File
+import scala.io.Source
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd._
+import org.apache.spark.mllib.recommendation.{ALS,Rating,MatrixFactorizationModel}
+import scala.collection.immutable.Map
+}
+object Recommend {
+  
+}
